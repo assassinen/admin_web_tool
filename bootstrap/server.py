a2643bda9fd9908ac1f4292ce072d6652ee61d7b
Ivan Novikov <i.novikov@iconic.vc>
2018-11-19 17:25:47 +0300

    fix press button

--- bootstrap/server.py
+++ bootstrap/server.py
@@ -33,5 +33,21 @@ def list():
            return "false"
    return data
 
+@app_test.route("/server/add")
+def add():
+    print("Recevied args: {0}".format(flask.request.args))
+    message_dict = flask.request.args.to_dict()
+    print("Message dict: {0}".format(message_dict))
+    with open("data.txt", "a+") as file_descriptor:
+        try:
+            element = json.dumps(message_dict)
+            print("Element will be writed in file: {0}".format(element))
+            file_descriptor.write(element)
+            file_descriptor.write('\n')
+            file_descriptor.close()
+        except Exception as e:
+            return "false"
+    return "true"
+
 if __name__ == "__main__":
     app_test.run(host='0.0.0.0', port=5000, debug=True)
